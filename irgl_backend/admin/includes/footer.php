991543dbbddd137e7d8531154926c391236ab144
Andreas Pandu Pamungkas <andspemail@gmail.com>
2022-03-25 14:21:32 +0700

    Delete irgl_backend directory

--- irgl_backend/admin/includes/footer.php
+++
@@ -1,5 +0,0 @@
-        <script src="<?=$pathURL?>assets/js/jquery-3.6.0.min.js"></script>
-        <script src="<?=$pathURL?>assets/js/popper.min.js"></script>
-        <script src="<?=$pathURL?>assets/js/bootstrap.min.js"></script>
-    </body>
-</html>
