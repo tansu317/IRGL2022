ca1a180483c5b2f050a3f829a61b52d63236eb8a
Andreas Pandu Pamungkas <andspemail@gmail.com>
2022-04-03 18:23:37 +0700

    Update reCaptcha Login

--- irgl_backend/backend/login.php
+++ irgl_backend/backend/login.php
@@ -9,64 +9,70 @@ $redirect       = '';
 if ($_SERVER["REQUEST_METHOD"] == 'POST')
 {
     $return = true;
+    $cek_captcha = verifikasiCaptcha($_POST['g-recaptcha-response'], $recaptcha_sck);
 
-    $email  = trim(strtolower($_POST['email']));
-    $pass   = $_POST['team_password'];
-
-    // Cek email user
-    $sql_ceu = "SELECT team_id FROM `2022_main_participants` WHERE email = :email";
-    $check_ceu = $pdo->prepare($sql_ceu);
-    $check_ceu->bindParam(':email', $email);
-    $check_ceu->execute();
-
-    if ($check_ceu->rowCount() > 0)
+    if ($cek_captcha)
     {
-        $fetch_ceu = $check_ceu->fetch(PDO::FETCH_OBJ);
+        $email  = trim(strtolower($_POST['email']));
+        $pass   = $_POST['team_password'];
 
-        // Cek Main Teams
-        $sql_cmt = "SELECT status, password FROM `2022_main_teams` WHERE id = :id";
-        $check_cmt = $pdo->prepare($sql_cmt);
-        $check_cmt->bindParam(':id', $fetch_ceu->team_id);
-        $check_cmt->execute();
+        // Cek email user
+        $sql_ceu = "SELECT team_id FROM `2022_main_participants` WHERE email = :email";
+        $check_ceu = $pdo->prepare($sql_ceu);
+        $check_ceu->bindParam(':email', $email);
+        $check_ceu->execute();
 
         if ($check_ceu->rowCount() > 0)
         {
-            $fetch_cmt = $check_cmt->fetch(PDO::FETCH_OBJ);
+            $fetch_ceu = $check_ceu->fetch(PDO::FETCH_OBJ);
 
-            if (!password_verify($pass, $fetch_cmt->password))
+            // Cek Main Teams
+            $sql_cmt = "SELECT status, password FROM `2022_main_teams` WHERE id = :id";
+            $check_cmt = $pdo->prepare($sql_cmt);
+            $check_cmt->bindParam(':id', $fetch_ceu->team_id);
+            $check_cmt->execute();
+
+            if ($check_ceu->rowCount() > 0)
             {
-                $msg = 'The team password you entered is wrong.';
+                $fetch_cmt = $check_cmt->fetch(PDO::FETCH_OBJ);
+
+                if (!password_verify($pass, $fetch_cmt->password))
+                {
+                    $msg = 'The team password you entered is wrong.';
+                }
+                else if ($fetch_cmt->status == 0)
+                {
+                    $msg = 'Your team is still not approved, please contact the Administrator.';
+                }
+                else 
+                {
+                    $login_token = md5($email.$pass.time().rand(0, 99999));
+                    $time_login = date('Y-m-d H:i:s');
+
+                    // Update Main Teams
+                    $_SESSION['login_team'] = $login_token;
+                    $sql_update_main = "UPDATE `2022_main_teams` SET `date_of_last_login` = '$time_login' WHERE id = :id";
+
+                    $update_main = $pdo->prepare($sql_update_main);
+                    $update_main->bindParam(':id', $fetch_ceu->team_id);
+                    $update_main->execute();
+
+                    // // redirect
+                    // header('location: ./coming_soon.php');
+
+                    $successLogin   = true;
+                    $redirect       = './index.php';
+                    $msg            = 'Successfully logged in, you will be redirected to a new page.';
+                }
             }
-            else if ($fetch_cmt->status == 0)
-            {
-                $msg = 'Your team is still not approved, please contact the Administrator.';
-            }
-            else 
-            {
-                $login_token = md5($email.$pass.time().rand(0, 99999));
-                $time_login = date('Y-m-d H:i:s');
-
-                // Update Main Teams
-                $_SESSION['login_team'] = $login_token;
-                $sql_update_main = "UPDATE `2022_main_teams` SET `date_of_last_login` = '$time_login' WHERE id = :id";
-
-                $update_main = $pdo->prepare($sql_update_main);
-                $update_main->bindParam(':id', $fetch_ceu->team_id);
-                $update_main->execute();
-
-                // // redirect
-                // header('location: ./coming_soon.php');
-
-                $successLogin   = true;
-                $redirect       = './index.php';
-                $msg            = 'Successfully logged in, you will be redirected to a new page.';
-            }
+            else
+                $msg = 'Your team is not registered, please contact the Administrator.';
         }
         else
-            $msg = 'Your team is not registered, please contact the Administrator.';
+            $msg = 'Your email is not registered.';
     }
-    else
-        $msg = 'Your email is not registered.';
+    else 
+        $msg = 'Please reload your page, to verify the captcha.';
 }
 
 header('Content-Type: application/json');
