ff28d48004966a8f39b328a418fb380502bcafe9
Andreas Pandu Pamungkas <andspemail@gmail.com>
2022-04-12 19:11:06 +0700

    Update API Data Peserta (json)

--- irgl_backend/admin/assets/js/main.js
+++ irgl_backend/admin/assets/js/main.js
@@ -6,18 +6,28 @@ $(function()
 
     $(document).on('click', 'button[data-toggle=verifikasi_peserta]', function()
     {
-        const id = $(this).attr('data-id');
-        const statusDiv = $('#status-id-'+id);
+        const id             = $(this).attr('data-id');
+        const statusDiv      = $('#status-id-'+id);
+        const verificationDiv = $('#verification-id-'+id);
 
         $.ajax({
             type: "POST",
             data: 'verifikasi=true&id='+id,
-            success:(data) =>
+            success: (data) =>
             {
-                if (data == 1)
-                    statusDiv.html(`<span class="badge badge-success">
-                                        <i class="fa fa-check-square-o"></i>
-                                    </span>`);
+                if (data.status == 1)
+                {
+                    statusDiv.html(`
+                        <span class="badge badge-success">
+                            <i class="fa fa-check-square-o"></i>
+                        </span>
+                    `);
+                    
+                    verificationDiv.html(`
+                        - ${data.verificator}<br/>
+                        - ${data.time}
+                    `);
+                }
                 else
                     alert('Data tidak ditemukan atau sudah terverifikasi');
             },
