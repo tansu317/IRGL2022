3bba9c1192c37f65f38a24ca4144c460b8d3e32d
Andreas Pandu Pamungkas <andspemail@gmail.com>
2022-04-12 19:12:01 +0700

    Data Peserta (verificator, date of verification)

--- irgl_backend/admin/irgl/data-team.php
+++ irgl_backend/admin/irgl/data-team.php
@@ -13,24 +13,45 @@ if (isset($_POST['verifikasi']) &&
     $cek_id->bindParam(':id', $id);
     $cek_id->execute();
 
-    $status = 0;
+    $status = ['status' => 0];
     if ($cek_id->rowCount() > 0)
     {
         $fetch_id = $cek_id->fetch();
 
         if ($fetch_id['status'] == 0)
         {
-            $update_status = "UPDATE `2022_main_teams` SET status = 1 WHERE id = :id";
+            $sql = 'SELECT nama FROM `2022_admin` WHERE login_cookie = :hash';
+            $fetch_admin = $pdo->prepare($sql);
+            $fetch_admin->bindParam(':hash', $cookie_login);
+            $fetch_admin->execute();
+
+            $fetch_admin = $fetch_admin->fetch(PDO::FETCH_OBJ);
+            $nama_admin = empty($fetch_admin->nama) ? $fetch_admin->nrp : $fetch_admin->nama;
+            $nama_admin = htmlspecialchars($nama_admin);
+
+            $time_verify = date('Y-m-d H:i:s');
+            
+            $update_status = "UPDATE `2022_main_teams` SET status = 1, verificator = :vc, date_of_verification = :dof WHERE id = :id";
             $update_status = $pdo->prepare($update_status);
             $update_status->bindParam(':id', $id);
+            $update_status->bindParam(':vc', $nama_admin);
+            $update_status->bindValue(':dof', $time_verify);
             $update_status->execute();
 
             if ($update_status)
-                $status = 1;
+            {
+                $json = [
+                    'status' => 1,
+                    'verificator' => $nama_admin,
+                    'time'  => $time_verify
+                ];
+            }
         }
     }
 
-    echo $status;
+    header('Content-Type: application/json');
+    echo json_encode($json);
+    die;
 }
 else 
 {
@@ -48,8 +69,10 @@ else
         <div class="content">
             <?php
 
-            $data_tim = "SELECT `id`, `name`, `participant_count`, `school`, `payment_filepath`, `date_of_registration`, `status` FROM
-            `2022_main_teams`";
+            $data_tim = 
+                "SELECT `id`, `name`, `participant_count`, `school`, 
+                        `payment_filepath`, `date_of_registration`, `status`, `verificator`, `date_of_verification` 
+                    FROM `2022_main_teams`";
             $data_tim_stmt = $pdo->prepare($data_tim);
             $data_tim_stmt->execute();
             
@@ -66,22 +89,40 @@ else
                                 <th scope="col">Payment Filepath</th>
                                 <th scope="col">Registration Date</th>
                                 <th scope="col">Status</th>
+                                <th scope="col">Verification</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php while($data = $data_tim_stmt->fetch())
+                                <?php while ($data = $data_tim_stmt->fetch())
                                 {
+                                    $verificator    = '-'.(!empty($data['verificator']) ? ' '.htmlspecialchars($data['verificator']) : '');
+                                    $dateVerify     = '-'.(strtotime($data['date_of_verification']) > 0 ? ' '.$data['date_of_verification'] : '');
+                                    $payment_path   = '/backend/uploads/bukti_transfer/'.htmlspecialchars($data['payment_filepath']);
+                                    $file_payment   = $_SERVER['DOCUMENT_ROOT'].$payment_path;
+
                                     echo "<tr>";
                                     echo "<td>" . htmlspecialchars($data['id']) . "</td>";
                                     echo "<td>" . htmlspecialchars($data['name']) . "</td>";
                                     echo "<td>" . htmlspecialchars($data['participant_count']) . "</td>";
                                     echo "<td>" . htmlspecialchars($data['school']) . "</td>";
-                                    echo '<td><a href="/backend/uploads/bukti_transfer/'.htmlspecialchars($data['payment_filepath']).'" target="_blank" class="btn btn-primary btn-sm">Lihat Bukti</td>';
-                                    echo "<td>" . htmlspecialchars($data['date_of_registration']) . "</td>";
+                                    echo '
+                                        <td>
+                                            '.(file_exists($file_payment) ? '<a href="'.$payment_path.'" target="_blank" class="btn btn-primary btn-sm">Lihat Bukti</a>' : 'Tidak dapat menemukan file').'
+                                        </td>';
+                                    
+                                    echo "<td>" . (strtotime($data['date_of_registration']) > 0 ? htmlspecialchars($data['date_of_registration']) : '-') . "</td>";
+                                    
                                     echo '
                                         <td style="text-align: center">
                                             <div id="status-id-'.$data['id'].'">
                                                 '.(strip_tags($data['status']) == 1 ? '<span class="badge badge-success"><i class="fa fa-check-square-o"></i></span>' : '<button data-toggle="verifikasi_peserta" data-id="'.$data['id'].'" class="btn btn-danger btn-sm">Verifikasi</button>').'</i>
+                                            </div>
+                                        </td>';
+
+                                    echo '
+                                        <td>
+                                            <div id="verification-id-'.$data['id'].'">
+                                                '.($verificator == '-' || $dateVerify == '-' ? '-' : $verificator.'<br/>'.$dateVerify).'
                                             </div>
                                         </td>';
                                     echo "</tr>";
