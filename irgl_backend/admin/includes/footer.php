5b78236d726f874338b48fc286949fe0bb8166c7
Andreas Pandu Pamungkas <andspemail@gmail.com>
2022-04-12 17:22:15 +0700

    Update DataTable.js sama Main.js

--- irgl_backend/admin/includes/footer.php
+++ irgl_backend/admin/includes/footer.php
@@ -1,5 +1,7 @@
         <script src="<?=$pathURL?>assets/js/jquery-3.6.0.min.js"></script>
         <script src="<?=$pathURL?>assets/js/popper.min.js"></script>
         <script src="<?=$pathURL?>assets/js/bootstrap.min.js"></script>
+        <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+        <script src="<?=$pathURL?>assets/js/main.js"></script>
     </body>
 </html>
